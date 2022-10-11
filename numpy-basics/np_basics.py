4ad3bade919c455839032b33427e746877dc3b17
Jakub Rutrle <rutrle@seznam.cz>
2022-10-11 19:27:38 +0200

    numpy basics

---
+++ numpy-basics/np_basics.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def try_arange():
+    a = np.arange(6)
+    
+    a2=a[np.newaxis, :]
+
+    print(a)
+    print(a.shape)
+
+    print(a2)
+    print(a2.shape)
+
+
+
+if __name__=="__main__":
+    try_arange()
